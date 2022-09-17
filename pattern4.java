4a6a65e9b0c0eae5ca66397dce31683d73336e9e
Prakash-lk <111338319+Prakash-lk@users.noreply.github.com>
2022-09-17 22:06:33 +0530

    Create pattern4.java

---
+++ pattern4.java
@@ -0,0 +1,31 @@
+# Patterns
+
+/*
+input : n=5
+        1 
+        2 3 
+        4 5 6 
+        7 8 9 10 
+        11 12 13 14 15
+*/
+
+#java
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    Scanner in=new Scanner(System.in);
+	    int n=in.nextInt();
+	    int temp=1;
+	    for(int row=1;row<=n;row++)
+	    {
+	        for(int col=1;col<=row;col++)
+	        {
+	            System.out.print((temp++)+ " ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
