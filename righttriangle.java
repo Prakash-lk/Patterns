d4e286eeadda9ff8b03206ef95e24ad9e5cbea33
Prakash-lk <111338319+Prakash-lk@users.noreply.github.com>
2022-09-17 20:17:12 +0530

    Create righttriangle.java

---
+++ righttriangle.java
@@ -0,0 +1,30 @@
+/*
+input: n=5
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
+
+#java
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    Scanner in=new Scanner(System.in);
+	    int n=in.nextInt();
+	    for(int row=1;row<=n;row++)
+	    {
+	        for(int col=1;col<=row;col++)
+	        {
+	            System.out.print("*" + " ");
+	        }
+	        System.out.println();
+	    }
+	}
+}
