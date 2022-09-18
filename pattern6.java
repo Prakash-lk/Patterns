d86718174583dbd0a608b0a9b274c991f3055756
Prakash-lk <111338319+Prakash-lk@users.noreply.github.com>
2022-09-18 19:20:20 +0530

    Create pattern6.java

---
+++ pattern6.java
@@ -0,0 +1,30 @@
+# Patterns
+
+/*
+input : n=5
+output:     * 
+          * * 
+        * * * 
+      * * * * 
+    * * * * *
+*/
+
+#java
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		for(int row=1;row<=n;row++){
+		    for(int sp=0;sp<n-row;sp++){
+		        System.out.print("  ");
+		    }
+		    for(int col=1;col<=row;col++){
+		        System.out.print("*"+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
