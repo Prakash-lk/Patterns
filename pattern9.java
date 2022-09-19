cbcf40e72cc7e74578359862ac647d4a3ae9ab4d
Prakash-lk <111338319+Prakash-lk@users.noreply.github.com>
2022-09-19 17:33:21 +0530

    Create pattern9.java

---
+++ pattern9.java
@@ -0,0 +1,31 @@
+# Patterns
+
+/*
+input : n=5
+output:
+      * * * * * 
+        * * * * 
+          * * * 
+            * * 
+              *
+*/
+
+#java
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		for(int row=1;row<=n;row++){
+		    for(int space=1;space<row;space++){
+		        System.out.print("  ");
+		    }
+		    for(int col=1;col<=n-row+1;col++){
+		        System.out.print("*"+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
